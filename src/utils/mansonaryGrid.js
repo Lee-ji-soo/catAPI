35ab9c767d0874151fcd7c2149d2ee8407792e9e
Lee-ji-soo <nbsp3@naver.com>
2020-11-03 01:18:26 +0900

    [Header.js] SCSS 수정

--- src/utils/mansonaryGrid.js
+++ src/utils/mansonaryGrid.js
@@ -2,8 +2,7 @@ const mansonaryGrid = (container, item) => {
     const rowHeight = parseInt(window.getComputedStyle(container).getPropertyValue('grid-auto-rows'));
     const rowGap = 10;
     const rowSpan = Math.floor((item.querySelector('img').offsetHeight) / (rowHeight + rowGap));
-    // const rowSpan = Math.ceil((item.querySelector('img').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
-    item.style.gridRowEnd = `span ${rowSpan + 1}`;
+    item.style.gridRowEnd = `span ${rowSpan + 2}`;
 }
 
 export { mansonaryGrid };
